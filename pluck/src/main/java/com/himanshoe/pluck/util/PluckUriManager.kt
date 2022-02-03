30a68ffe46ffc39c745eda9541e13ff55207a323
Himanshu Singh <hello2himanshusingh@gmail.com>
2022-02-03 02:46:02 +0100

    [Tooling] Added Depandabot + Workflows + License

---
+++ pluck/src/main/java/com/himanshoe/pluck/util/PluckUriManager.kt
@@ -0,0 +1,65 @@
+package com.himanshoe.pluck.util
+/*
+* MIT License
+*
+* Copyright (c) 2022 Himanshu Singh
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import com.himanshoe.pluck.data.PluckImage
+
+internal class PluckUriManager(private val context: Context) {
+
+    private val photoCollection by lazy {
+        if (Build.VERSION.SDK_INT > 28) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+    }
+
+    private val resolver by lazy { context.contentResolver }
+
+    val newUri = resolver.insert(photoCollection, setupPhotoDetails())
+
+    /**
+     * generates a new pluck image
+     */
+    fun getPluckImage(uri: Uri?): PluckImage? = uri?.let {
+        PluckImage(
+            it,
+            System.currentTimeMillis(),
+            setupPhotoDetails().getAsString(MediaStore.Images.Media.DISPLAY_NAME), null, null
+        )
+    }
+    /**
+     * generates a photo detail
+     */
+    private fun setupPhotoDetails() = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, getFileName())
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+    }
+
+    private fun getFileName() = "pluck-camera-${System.currentTimeMillis()}.jpg"
+}
