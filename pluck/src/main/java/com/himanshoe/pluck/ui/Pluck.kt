91dd8f35469c2e7ed8c3d3787cbd702912ac3063
hongxiang <huxhu@thoughtworks.com>
2022-07-23 12:14:46 +0800

    Fix the kotlin import issue

--- pluck/src/main/java/com/himanshoe/pluck/ui/Pluck.kt
+++ pluck/src/main/java/com/himanshoe/pluck/ui/Pluck.kt
@@ -30,7 +30,11 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
